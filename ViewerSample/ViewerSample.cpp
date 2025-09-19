ae09dfe8d8d8ed068dd6cf4b90d97eed5699162b
ej.yoon <yuneunlx@gmail.com>
2025-09-19 10:20:58 +0900

    프레임워크 추가

--- ViewerSample/ViewerSample.cpp
+++ ViewerSample/ViewerSample.cpp
@@ -1,10 +1,122 @@
 #include "ViewerSample.h"
 
-ViewerSample::ViewerSample(QWidget *parent)
-    : QMainWindow(parent)
+//위젯의 외형 커스터마이징
+//버튼, 체크박스, 스크롤바 등 기본 위젯의 모양을 직접 그릴 수 있음
+#include <QStyle>
+
+//디버깅 메시지 출력
+#include <QDebug>
+
+//현재 시간 가져오기
+#include <QTime>
+
+//Qt에서 디스플레이 관련 정보를 가져오거나 제어할 때 사용하는 헤더
+#include <QScreen>
+
+//Qt에서 팝업 메시지 창(대화상자) 띄울 때 사용하는 헤더
+#include <QMessageBox>
+
+//Qt에서 창이 닫힐 때 발생하는 이벤트를 처리하기 위해 사용하는 헤더
+#include <QCloseEvent>
+
+//Qt에서 화면(디스플레이)정보에 접근하기 위해 사용되던 헤더
+#include <QDesktopWidget>
+
+ViewerSample::ViewerSample(QWidget * parent)
+	: QMainWindow(parent)
+	, ui(new Ui::ViewerSampleClass)
+
+
+	, m_WindowSize(QSize(1280, 800))
+
+	//체크박스 위젯을 가리키는 포인터
+	, m_pCbxDoFrames(new QCheckBox(this))
 {
-    ui.setupUi(this);
+	//setupUi(this)는 .ui 파일에 정의된 모든 위젯을 this (ex: QMainWindow)에 붙이고 초기화 작업
+	ui->setupUi(this);
+
+
+	m_pScene = ui->view;
+
+	adjustWindowSize();
+	addToolbarWidgets();
+	connectSlots();
 }
 
-ViewerSample::~ViewerSample()
-{}
+ViewerSample::~ViewerSample() = default;
+
+void ViewerSample::adjustWindowSize()
+{
+	resize(m_WindowSize.width(), m_WindowSize.height());
+	setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, size(),
+		qApp->screens().first()->availableGeometry()));
+}
+
+void ViewerSample::addToolbarWidgets()
+{
+	// Add CheckBox to tool-bar to stop/continue frames execution.
+	m_pCbxDoFrames->setText("Do Frames");
+	m_pCbxDoFrames->setChecked(true);
+	connect(m_pCbxDoFrames, &QCheckBox::stateChanged, [&] {
+		if (m_pCbxDoFrames->isChecked())
+			m_pScene->continueFrames();
+		else
+			m_pScene->pauseFrames();
+		});
+	ui->mainToolBar->addWidget(m_pCbxDoFrames);
+}
+
+void ViewerSample::connectSlots()
+{
+	connect(m_pScene, &QDirect3D11Widget::deviceInitialized, this, &ViewerSample::init);
+	connect(m_pScene, &QDirect3D11Widget::ticked, this, &ViewerSample::tick);
+	connect(m_pScene, &QDirect3D11Widget::rendered, this, &ViewerSample::render);
+
+	// NOTE: Additionally, you can listen to some basic IO events.
+	// connect(m_pScene, &QDirect3D11Widget::keyPressed, this, &ViewerSample::onKeyPressed);
+	// connect(m_pScene, &QDirect3D11Widget::mouseMoved, this, &ViewerSample::onMouseMoved);
+	// connect(m_pScene, &QDirect3D11Widget::mouseClicked, this, &ViewerSample::onMouseClicked);
+	// connect(m_pScene, &QDirect3D11Widget::mouseReleased, this,
+	// &ViewerSample::onMouseReleased);
+}
+
+void ViewerSample::init(bool success)
+{
+	if (!success)
+	{
+		QMessageBox::critical(this, "ERROR", "Direct3D widget initialization failed.",
+			QMessageBox::Ok);
+		return;
+	}
+
+	// TODO: Add here your extra initialization here.
+	// ...
+
+	// Start processing frames with a short delay in case things are still initializing/loading
+	// in the background.
+	QTimer::singleShot(500, this, [&] { m_pScene->run(); });
+	disconnect(m_pScene, &QDirect3D11Widget::deviceInitialized, this, &ViewerSample::init);
+}
+
+void ViewerSample::tick()
+{
+	// TODO: Update the scene here.
+	// m_pMesh->Tick();
+}
+
+void ViewerSample::render()
+{
+	// TODO: Present the scene here.
+	// m_pMesh->Render();
+}
+
+void ViewerSample::closeEvent(QCloseEvent * event)
+{
+	event->ignore();
+	m_pScene->release();
+	QTime dieTime = QTime::currentTime().addMSecs(500);
+	while (QTime::currentTime() < dieTime)
+		QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
+
+	event->accept();
+}
--- ViewerSample/main.cpp
+++ ViewerSample/main.cpp
@@ -48,8 +48,6 @@ public:
 			//가로 : col
 			int col = i % 2;
 			layout->addWidget(view, row, col);
-			
-
 		}
 
 		setCentralWidget(central);
@@ -57,15 +55,26 @@ public:
 		resize(800, 600);
 	}
 };
+
+
+//Qt의 이벤트 루프를 시작하는 함수
+//마우스 클릭, 키 입력, 윈도우 리사이즈 같은 이벤트를 처리하면서 앱이 계속 실행됨
+//a.exec()이 끝나면 앱이 종료됨=>main() 함수가 반환됨
+//QApplication 생성 => FourViewWindow 생성 => show() 호출->창 띄움
+//=> a.exec() -> 이벤트 루프 시작 => 사용자 이벤트 처리 => 창 닫힘->exec() 종료 
+//=> return->프로그램 종료
 int main(int argc, char *argv[])
 {
+	//qt gui 애플리케이션을 초기화하는 객체
+	//이벤트 루프 관리 및 모든 위젯의 생명주기를 통제함
     QApplication a(argc, argv);
 
-    /*ViewerSample w;
-    w.show();*/
+    ViewerSample w;
+    w.show();
 
-	FourViewWindow w;
-	w.show();
+	////QMainWindow를 상송한 클래스
+	//FourViewWindow w;
+	//w.show();
 
     return a.exec();
 }
